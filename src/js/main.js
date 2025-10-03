b90d203d3b69db5bfd2688a6f6000561d96ea155
stillistica <rozhkovavika24@gmail.com>
2025-10-03 18:00:52 +0300

    add form element

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,2 @@
-// import "./modules/scroll.js";
+import "./modules/form.js";
 
---
+++ src/js/modules/form.js
@@ -0,0 +1,29 @@
+const btnForm = document.querySelector(".form button");
+
+if (btnForm) {
+  btnForm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const form = document.querySelector(".form");
+    const inputs = form.querySelectorAll("input");
+
+    inputs.forEach((i) => {
+      const div = i.closest(".form__input");
+      const err = i.nextElementSibling;
+
+      if (err) {
+        err.remove();
+      }
+
+      if (!i.value.trim()) {
+        i.classList.add("err");
+        const msg = document.createElement("div");
+        msg.classList.add("err-mess");
+        msg.innerText = "Please fill out this field.";
+        div.appendChild(msg);
+      } else {
+        i.classList.remove("err");
+      }
+    });
+  });
+}
