4765ca0873d3fdc78ad1362d1582c44554d5700f
stillistica <rozhkovavika24@gmail.com>
2025-10-04 15:18:51 +0300

    add menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,4 @@
 import "./modules/form.js";
+import "./modules/menu.js";
+import "./modules/call.js";
 
---
+++ src/js/modules/menu.js
@@ -0,0 +1,20 @@
+const btnMenu = document.querySelector(".header__menu");
+const menu = document.querySelector(".menu");
+
+if (btnMenu && menu) {
+  btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    document.querySelectorAll(".menu__nav-item").forEach((e) => {
+      e.addEventListener("click", () => {
+        menu.classList.remove("active");
+        document.body.style.overflow = "";
+      });
+    });
+  });
+}
