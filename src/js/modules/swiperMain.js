f313f517c7e95051965ccf54108b5eee51c1bbaa
stillistica <rozhkovavika24@gmail.com>
2025-10-15 12:41:13 +0300

    update swiper main

--- src/js/modules/swiperMain.js
+++ src/js/modules/swiperMain.js
@@ -1,5 +1,5 @@
 import Swiper from "swiper/bundle";
-import { Pagination, Keyboard, Mousewheel } from "swiper/modules";
+import { Pagination, Keyboard } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -12,16 +12,17 @@ if (swiperMain) {
       swiperMain = new Swiper(".swiper-main", {
         direction: "vertical",
         effect: "fade",
-        mousewheel: {
-          forceToAxis: true, 
-        },
         keyboard: { enabled: true, onlyInViewport: true },
-        modules: [Pagination, Keyboard, Mousewheel],
+        modules: [Pagination, Keyboard],
         slidesPerView: 1,
         resizeObserver: true,
         observer: true,
         observeParents: true,
         loop: false,
+        navigation: {
+          nextEl: ".swiper-button-next-main",
+          prevEl: ".swiper-button-prev-main",
+        },
         pagination: {
           el: ".swiper-pagination-main",
           clickable: true,
@@ -37,7 +38,38 @@ if (swiperMain) {
             </div>`;
           },
         },
+        on: {
+          init: function () {
+            toggleNavButtons(this);
+          },
+          slideChange: function () {
+            toggleNavButtons(this);
+          },
+        },
       });
+
+      function toggleNavButtons(swiper) {
+        const prevBtn = document.querySelector(".swiper-button-prev-main");
+        const nextBtn = document.querySelector(".swiper-button-next-main");
+
+        // якщо це перший слайд — ховаємо prev
+        if (swiper.activeIndex === 0) {
+          prevBtn.style.opacity = "0";
+          prevBtn.style.pointerEvents = "none";
+        } else {
+          prevBtn.style.opacity = "1";
+          prevBtn.style.pointerEvents = "auto";
+        }
+
+        // якщо це останній слайд — ховаємо next
+        if (swiper.activeIndex === swiper.slides.length - 1) {
+          nextBtn.style.opacity = "0";
+          nextBtn.style.pointerEvents = "none";
+        } else {
+          nextBtn.style.opacity = "1";
+          nextBtn.style.pointerEvents = "auto";
+        }
+      }
     } else if (window.innerWidth < 1440 && swiperMain) {
       swiperMain.destroy(true, true);
       swiperMain = null;
